f8608d487a4eed089ec2e83fb749a738ace42ae0
LuizGustavoNascimeto <luizgnf1@gmail.com>
2026-04-23 16:24:19 -0300

    datagram definition

--- internal/datagram/datagram.go
+++ internal/datagram/datagram.go
@@ -1,5 +1,7 @@
 package datagram
 
+import "fmt"
+
 type MessageType int8
 
 const (
@@ -14,5 +16,36 @@ type Datagram struct {
 	NickSize    uint8
 	Nick        string
 	MessageSize uint8
-	Message     string
+	MessageText string
+}
+
+func ParseDatagram(data []byte) (*Datagram, error) {
+	//represents the current position in the datagram
+	var pos int = 0
+
+	if len(data) < 4 {
+		return nil, fmt.Errorf("INVALID_DATAGRAM: datagram must be at least 3 bytes long")
+	}
+	//set o nicksize e o messageType
+	dg := &Datagram{
+		MessageType: MessageType(data[0]),
+		NickSize:    data[1],
+	}
+	pos += 2
+
+	//set o nick
+	if dg.NickSize > 0 && dg.NickSize <= 64 {
+		dg.Nick = string(data[pos : pos+int(dg.NickSize)])
+		pos += int(dg.NickSize)
+	} else {
+		return nil, fmt.Errorf("INVALID_DATAGRAM: invalid nickname size")
+	}
+
+	//set o messagesize e o messagetext
+	dg.MessageSize = data[pos]
+	pos++
+	dg.MessageText = string(data[pos : pos+int(dg.MessageSize)])
+
+	return dg, nil
+
 }
